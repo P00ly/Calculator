b15e6c0596c0b6413236be3c3520184ea1582aa1
P00LY <alexjmarsh55@gmail.com>
2022-03-30 12:19:17 -0700

    add subtraction function

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-//Add function
+//Addition function
 function addition(...num) {
     return num.reduce((previous, current) => {
         return previous + current;
     });
 };
+
+console.log(addition(9, 9, "addition"));
+
+//subtraction function
+function subtraction(...num) {
+    return num.reduce((previous, current) => {
+        return previous - current;
+    });
+};
+
+console.log(subtraction(9, 6));
+
 
