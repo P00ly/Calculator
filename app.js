98997f1a6b56b81c7dcac4db67200869a25a01f0
P00LY <alexjmarsh55@gmail.com>
2022-03-31 11:18:56 -0700

    add multiplication and divison function

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 //Addition function
-function addition(...num) {
+function add(...num) {
     return num.reduce((previous, current) => {
         return previous + current;
     });
 };
 
-console.log(addition(9, 9, "addition"));
+console.log(add(9, 9, "addition"));
 
 //subtraction function
-function subtraction(...num) {
+function subtract(...num) {
     return num.reduce((previous, current) => {
         return previous - current;
     });
 };
 
-console.log(subtraction(9, 6));
+console.log(subtract(9, 6));
 
+//multiplication function
 
+function multiply(...num) {
+    return num.reduce((total, current) => total * current);
+}
+
+console.log(multiply(2, 2, 4));
+
+//Division function
+function divide(...num) {
+    return num.reduce((total, current) => total / current);
+}
+
+console.log(divide(16, 2, 2, 2));
+
+//Operate
+function operate(operator, num1, num2) {
+    
+}
